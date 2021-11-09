61173eb6f21a3bf046fb7bdc5c1b1490a5f26f70
Felix Matschke <felix.matschke@icloud.com>
2021-11-09 15:12:47 -0500

    changes to global

--- Globals.py
+++ Globals.py
@@ -7,3 +7,8 @@ origin = tempPoint()
 yzSketch = None
 xzSketch = None
 xySketch = None
+
+
+# oder sollte ich das in contextvars machen?
+
+# https://stackoverflow.com/questions/50854974/context-variables-in-python/50856376#50856376
